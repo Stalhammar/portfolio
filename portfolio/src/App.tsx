9d512318d541135eed1e6631257050ababf76439
Stalhammar <162510466+Stalhammar@users.noreply.github.com>
2026-03-30 21:27:44 +0200

    Add social icons and links

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,27 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 function App() {
   return (
-    <div className="flex items-center justify-center p-18">
-      <h1 className="font-serif text-5xl font-bold text-center">Ludvig Stålhammar</h1> 
+    <div className="min-h-screen flex flex-col items-center">
+      <h1 className="font-serif text-5xl font-bold text-center p-18">
+        Ludvig Stålhammar
+      </h1>
+      <div className="flex gap-8">
+        <a
+          href="https://github.com/Stalhammar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="w-12 h-12 cursor-pointer transition transform hover:scale-105" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ludvig-stalhammar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="w-12 h-12 cursor-pointer transition transform hover:scale-105" />
+        </a>
+      </div>
     </div>
   );
 }
